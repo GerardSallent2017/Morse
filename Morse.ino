9ffaab5ac232f954e8ca1afe1815d3c603cc6347
GerardSallent2017 <36242072+GerardSallent2017@users.noreply.github.com>
2018-05-23 22:42:00 +0200

    morse

---
+++ Morse.ino
@@ -0,0 +1,71 @@
+/******************************************************************************
+**                                                                           **
+**                              Morse                                        **
+**                                                                           **
+**                                                                           **
+******************************************************************************/
+
+//****** Includes *************************************************************
+
+//****** Variables ************************************************************
+const int pin = 8;   // pin for speaker
+
+int note = 1047;
+int velocitat = 10;
+
+//****** Setup ****************************************************************
+void setup()
+{
+}
+
+//****** Loop *****************************************************************
+void loop()
+{
+  //S   ** Exemple SOS **
+  punt();
+  ratlla();
+  ratlla();
+  punt();
+  espaiL() ;
+  punt();
+  ratlla();
+  espaiL();
+  ratlla();
+  espaiL();
+   punt();
+  ratlla();
+  espaiL();
+  ratlla();
+  espaiL();
+  punt();
+  ratlla();
+  espaiP();
+  
+
+}
+
+//****** Funcions *************************************************************
+void punt()
+{
+  tone(pin, note, 1000/velocitat);
+  delay(1000/velocitat); 
+  delay(1000/velocitat); 
+}
+
+void ratlla()
+{
+  tone(pin, note, 3*(1000/velocitat));
+  delay(3*(1000/velocitat)); 
+  delay(1000/velocitat);  
+}
+
+void espaiL()  //Espai entre lletres
+{  
+  delay(2*(1000/velocitat));  
+}
+
+void espaiP()  //Espai entre paraules
+{   
+  delay(6*(1000/velocitat));  
+}
+
